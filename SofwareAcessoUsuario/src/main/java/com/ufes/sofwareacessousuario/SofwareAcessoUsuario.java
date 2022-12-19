4d14914f838852bd2060e670dd584caabb78a959
heflainrmendes <heflain@outlook.com.br>
2022-12-19 16:37:21 -0300

    termino das views

---
+++ SofwareAcessoUsuario/src/main/java/com/ufes/sofwareacessousuario/SofwareAcessoUsuario.java
@@ -0,0 +1,43 @@
+package com.ufes.sofwareacessousuario;
+
+import com.ufes.sofwareacessousuario.view.LoginView;
+import com.ufes.sofwareacessousuario.view.NotificationsView;
+import com.ufes.sofwareacessousuario.view.PrincipalView;
+import com.ufes.sofwareacessousuario.view.InformationUserView;
+import javax.management.Notification;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
+/**
+ *
+ * @author heflainrmendes
+ */
+public class SofwareAcessoUsuario {
+
+    public static void main(String[] args) {
+//        try {
+//            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (Exception e) {
+//            // If Nimbus is not available, you can set the GUI to another look and feel.
+//        }
+        PrincipalView principal = new PrincipalView();
+//          LoginView login = new LoginView();  
+//          principal.add(login);
+//          login.setVisible(true);
+
+//          InformationUserView red = new InformationUserView();
+//          principal.add(red);
+//          red.setVisible(true);
+
+//            NotificationsView notificatios = new NotificationsView();
+//            principal.add(notificatios);
+//            notificatios.setVisible(true);
+
+            
+    }
+}
